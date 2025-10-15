16d76a992c1bd3034e91ba97a6ffc01d39710c1c
Mustafa Abu Eisha <169391469+AbuEisha@users.noreply.github.com>
2025-10-15 18:25:22 +0300

    Adding Shop Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import classes from "../styles/components/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <p>&copy; 2025 Sasa Shop. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
