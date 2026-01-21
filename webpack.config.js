b95a1aa82979d6f2503beec3eb2848c5e612cb31
AbuEisha <mostafaabuasha321@gmail.com>
2026-01-21 19:35:13 +0200

    Updating webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,33 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/js/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: argv.mode || "development",
+    entry: "./src/js/index.js",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    devtool: isProduction ? false : "eval-source-map",
+    devServer: {
+      watchFiles: ["./src/template.html"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+      }),
     ],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+  };
 };
